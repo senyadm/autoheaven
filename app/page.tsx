7ad168179ffb35500027c59a589ef4024f13e8bf
nurbekakbolat9 <nurbekakbolat9@gmail.com>
2023-08-21 21:57:02 +0600

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import Image from 'next/image'
-
+import {Navbar} from './pages/main'
 export default function Home() {
-  return <h1>Home</h1>
+  return <h1>
+
+
+    <Navbar/>
+  </h1>
 }
---
+++ app/pages/main.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import * as React from "react";
+import Link from "next/link";
+import {  EnterIcon, MagnifyingGlassIcon} from "@radix-ui/react-icons"; // Import Radix UI icons
+import { cn } from "@/lib/utils";
+import {InputField} from "@/app/components/InputField"
+import {
+    TfiAnnouncement,
+  } from "react-icons/tfi";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/app/components/Navbar";
+
+export function Navbar() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        {/* Logo */}
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>My App</NavigationMenuTrigger>
+      
+        </NavigationMenuItem>
+        {/* Search Input */}
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>
+            <MagnifyingGlassIcon /> 
+            <InputField/>
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            {/* Your search input component */}
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        {/* User Actions */}
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>
+            <TfiAnnouncement /> User
+          </NavigationMenuTrigger>
+        
+        </NavigationMenuItem>
+        {/* Authentication */}
+        <NavigationMenuItem>
+          <Link href="/login" passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <EnterIcon /> Login
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
