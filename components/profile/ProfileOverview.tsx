3daf496afdd8801e77882703ce593eef13f3b217
nurbekakbolat9 <nurbekakbolat9@gmail.com>
2023-10-06 01:04:05 +0600

    overview is done

--- components/profile/ProfileOverview.tsx
+++ components/profile/ProfileOverview.tsx
@@ -6,31 +6,43 @@ import { Label } from '../ui/label';
 const ProfileOverview = () => {
     return (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4'>
-            <Card className="w-[232.667px] h-[174px]">
-            <CardContent className='flex flex-col p-6 items-start '>
+ <Card className="relative w-[232.667px] h-[174px] rounded-lg bg-white overflow-hidden border-none shadow-none">
+
+    <svg className="absolute inset-0 w-full h-full" xmlns="http://www.w3.org/2000/svg">
+        <rect x="0" y="0" width="100%" height="100%" fill="none" stroke="#2563EB" strokeWidth="3" strokeDasharray="18,4" />
+    </svg>  <CardContent className='flex flex-col p-6 items-start '>
                     <Search size={64} color='#2563EB'/>
                     <Label className='mt-3 text-foreground font-bold'>Start a new search</Label>
                     <Label className='text-foreground text-sm'>2341 Vehicles available</Label>
                 </CardContent>
             </Card>
             
-            <Card className="w-[232.667px] h-[174px]">
-                <CardContent className='flex flex-col p-6 items-start'>
+            <Card className="relative w-[232.667px] h-[174px] rounded-lg bg-white overflow-hidden border-none shadow-none">
+
+<svg className="absolute inset-0 w-full h-full" xmlns="http://www.w3.org/2000/svg">
+    <rect x="0" y="0" width="100%" height="100%" fill="none" stroke="#2563EB" strokeWidth="3" strokeDasharray="18,4" />
+</svg>  <CardContent className='flex flex-col p-6 items-start'>
                     <ActivitySquare size={64} color='#2563EB'/>
                     <Label className='mt-3 text-foreground font-bold'>Show my active ads</Label>
                     <Label className='text-foreground text-sm'>2341 Vehicles available</Label>
                 </CardContent>
             </Card>
-            <Card className="w-[232.667px] h-[174px]">
-                <CardContent className='flex flex-col p-6 items-start'>
+            <Card className="relative w-[232.667px] h-[174px] rounded-lg bg-white overflow-hidden border-none shadow-none">
+
+<svg className="absolute inset-0 w-full h-full" xmlns="http://www.w3.org/2000/svg">
+    <rect x="0" y="0" width="100%" height="100%" fill="none" stroke="#2563EB" strokeWidth="4" strokeDasharray="18,4" />
+</svg>    <CardContent className='flex flex-col p-6 items-start'>
                     <Mail size={64} color='#2563EB'/>
                     <Label className='mt-3 text-foreground font-bold'>Inbox</Label>
                     <Label className='text-foreground text-sm'>2341 Vehicles available</Label>
                 </CardContent>
             </Card>
 
-            <Card className="w-[232.667px] h-[174px]">
-                <CardContent className='flex flex-col p-6 items-start'>
+            <Card className="relative w-[232.667px] h-[174px] rounded-lg bg-white overflow-hidden border-none shadow-none">
+
+<svg className="absolute inset-0 w-full h-full" xmlns="http://www.w3.org/2000/svg">
+    <rect x="0" y="0" width="100%" height="100%" fill="none" stroke="#2563EB" strokeWidth="3" strokeDasharray="18,4" />
+</svg>  <CardContent className='flex flex-col p-6 items-start'>
                     <Bookmark size={64} color='#2563EB'/>
                     <Label className='mt-3 text-foreground font-bold'>Saved</Label>
                     <Label className='text-foreground text-sm'>2341 Vehicles available</Label>
