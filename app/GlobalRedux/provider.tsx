ac86522dc22779c459ac5760202a303ea0bf5365
AberonAmarite <abay.darmenov@gmail.com>
2024-04-17 13:51:24 +0500

    add redux persist to store redux in local storage

--- app/GlobalRedux/provider.tsx
+++ app/GlobalRedux/provider.tsx
@@ -3,7 +3,8 @@
 import { Provider } from "react-redux";
 import { createWrapper } from "next-redux-wrapper";
 import React, { ReactNode } from "react";
-import { store } from "./store";
+import { persistor, store } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 interface ProvidersProps {
   children: ReactNode;
@@ -14,5 +15,9 @@ const makeStore = () => store;
 export const wrapper = createWrapper(makeStore);
 
 export const Providers = ({ children }: ProvidersProps) => (
-  <Provider store={store}>{children}</Provider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      {children}
+    </PersistGate>
+  </Provider>
 );
--- app/GlobalRedux/store.ts
+++ app/GlobalRedux/store.ts
@@ -9,19 +9,34 @@ import profileReducer from "./profile/profileSlice";
 import carCreationSlice from "./CreateCar/CreateCarSlice";
 
 import { chatSlice } from "./profile/chatSlice";
+import { combineReducers, createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
-export const store = configureStore({
-  reducer: {
-    carMakes: carMakesReducer,
-    carFiltersAndResults: carFiltersAndResultsReducer,
-    transportCategory: transportCategoryReducer,
-    profileNavigationMenu: profileNavigationMenuReducer,
-    user: userReducer,
-    profile: profileReducer,
-    createCarProgress: carCreationSlice,
-    chats: chatSlice.reducer,
-  },
+const rootReducer = combineReducers({
+  carMakes: carMakesReducer,
+  carFiltersAndResults: carFiltersAndResultsReducer,
+  transportCategory: transportCategoryReducer,
+  profileNavigationMenu: profileNavigationMenuReducer,
+  user: userReducer,
+  profile: profileReducer,
+  createCarProgress: carCreationSlice,
+  chats: chatSlice.reducer,
 });
+
+// export const store = configureStore({
+//   reducer: rootReducer,
+// });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({ reducer: persistedReducer });
+const persistor = persistStore(store);
+export { store, persistor };
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
