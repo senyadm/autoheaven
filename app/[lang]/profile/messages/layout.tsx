c49a484901b376543add660599be7ab231d74d74
AberonAmarite <abay.darmenov@gmail.com>
2024-04-21 21:16:23 +0500

    restructure messages again to simplify

--- app/[lang]/profile/messages/layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ProfileMessages from "../../../../components/profile/messages/ProfileMessages";
-
-const layout = ({ children }) => {
-  return <ProfileMessages>{children}</ProfileMessages>;
-};
-
-export default layout;
--- components/profile/messages/ChatMessagesContent.tsx
+++ components/profile/messages/ChatMessagesContent.tsx
@@ -1,52 +1,24 @@
 "use client";
 import { Divide } from "lucide-react";
 import React, { useEffect, useMemo } from "react";
-import { useAppSelector } from "../../../app/GlobalRedux/store";
+import { useAppDispatch, useAppSelector } from "../../../app/GlobalRedux/store";
 import { ChatMessageAPI } from "../../../interfaces/profile/messages";
-interface ChatMessagesContentProps {
-  ws: WebSocket | null;
-}
-const ChatMessagesContent = ({ ws }: ChatMessagesContentProps) => {
+import {
+  fetchChatMessages,
+  setCurrentChat,
+} from "../../../app/GlobalRedux/profile/chatSlice";
+import { useRouter } from "next/router";
+interface ChatMessagesContentProps {}
+const ChatMessagesContent = () => {
+  const dispatch = useAppDispatch();
   const currentChat = useAppSelector((state) => state.chats.currentChat);
   const currentMessages: ChatMessageAPI[] = useAppSelector(
     (state) => state.chats.currentChatMessages
   );
+  const userChats = useAppSelector((state) => state.chats.chats);
   const userId = useAppSelector((state) => state.user.id);
   let firstMessageYou = true,
     firstMessageResponder = true;
-  useEffect(() => {
-    if (!currentChat) return;
-    console.log(localStorage);
-    if (!ws) return;
-    ws.onopen = () => {
-      console.log("WebSocket connected");
-    };
-
-    ws.onmessage = (event: MessageEvent) => {
-      const newMessage = {
-        chat_id: new Date().getTime(),
-        message_content: event.data,
-        message_id: new Date().getTime(),
-        read_status: true,
-        sender_id: currentChat.chatter_id,
-        timestamp: new Date().toISOString(),
-      };
-      console.log("Received message:", newMessage);
-    };
-
-    ws.onerror = (error: Event) => {
-      console.error("WebSocket error:", error);
-    };
-
-    ws.onclose = () => {
-      console.log("WebSocket connection closed");
-    };
-
-    return () => {
-      ws.close();
-    };
-  }, [currentChat, ws]);
-
   return (
     <div className="flex flex-col w-full h-full px-4 py-2">
       {currentMessages.map((message, index) => {
@@ -65,9 +37,12 @@ const ChatMessagesContent = ({ ws }: ChatMessagesContentProps) => {
           firstMessageResponder = false;
           secondStr = "" + message.sender_id;
         }
-        const areYouSender = userId === message.sender_id;
+        const areYouSender = userId == message.sender_id;
         return (
-          <>
+          <div
+            className={`flex w-full ${areYouSender ? "" : ""}`}
+            key={message.message_id}
+          >
             <div className="text-sm text-muted-foreground mb-1">
               <div className="flex justify-end">{firstStr}</div>
               <div>{secondStr}</div>
@@ -76,25 +51,27 @@ const ChatMessagesContent = ({ ws }: ChatMessagesContentProps) => {
             <div
               key={index}
               className={`w-5/12 space-y-1 rounded-lg border pr-2 py-2 pl-4 ${
-                areYouSender ? "ml-auto text-foreground" : "bg-primary"
+                areYouSender ? "ml-auto text-foreground bg-primary" : ""
               }`}
             >
               <div
                 className={`text-sm ${
-                  areYouSender ? "text-foreground" : "text-primary-foreground"
+                  areYouSender ? "text-primary-foreground" : "text-foreground"
                 }`}
               >
                 {message.message_content}
               </div>
               <div
                 className={`flex justify-end text-[10px] ${
-                  areYouSender ? "text-muted-foreground" : "text-muted"
+                  areYouSender
+                    ? "text-primary-foreground"
+                    : "text-muted-foreground"
                 }`}
               >
-                {/* {message.timestamp.toISOString()} */}
+                {new Date(message.created_at).toLocaleTimeString()}
               </div>
             </div>
-          </>
+          </div>
         );
       })}
     </div>
