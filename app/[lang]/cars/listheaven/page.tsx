d7f13dafc9dd16df0459b410c5de58ce6d6fbdab
AberonAmarite <abay.darmenov@gmail.com>
2024-05-31 15:42:13 +0500

    add a svgr package for better svg import

---
+++ app/[lang]/cars/listheaven/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CarIcon from "@/public/icons/vehicle-category/car.svg";
+const page = () => {
+  return (
+    <div>
+      <CarIcon />
+    </div>
+  );
+};
+
+export default page;
